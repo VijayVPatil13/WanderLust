6c14b95c8f1e7ea96eca96f8dcb9f45488bb33fb
VijayVPatil13 <vijayvpatil13@gmail.com>
2026-02-24 20:02:13 +0530

    Connection to Database successfull

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const Listing = require('./models/listing'); 
-require('./models/db'); 
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/listingsDB";
+
+mongoose.connect(MONGO_URL)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error("MongoDB Error:", err));
 
 const path = require('path');
 const methodOverride = require('method-override');
@@ -17,7 +23,6 @@ app.use(methodOverride('_method'));
 
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
-
 
 // Redirect root
 app.get('/', (req, res) => {
--- models/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/listingsDB";
-
-mongoose.connect(MONGO_URL)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.error("MongoDB Error:", err));
-
-module.exports = mongoose;
