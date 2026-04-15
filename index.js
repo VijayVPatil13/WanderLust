8c2d161cbd9971201977ff24f90ba79653146fce
VijayVPatil13 <vijayvpatil13@gmail.com>
2026-04-15 22:46:01 +0530

    Fixed cookie settings

--- index.js
+++ index.js
@@ -70,8 +70,7 @@ const sessionOptions = {
   cookie: {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production"
+    httpOnly: true
   }
 };
 
